e4a6c6e782440ef84771a324ab76be67890c675a
cholokei <leesl0416@naver.com>
2013-02-23 19:52:35 +0900

    Gov: Add Asswax cpufreq governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -421,6 +421,9 @@ extern struct cpufreq_governor cpufreq_gov_abyssplug;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ADAPTIVE)
 extern struct cpufreq_governor cpufreq_gov_adaptive;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_adaptive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ASSWAX)
+extern struct cpufreq_governor cpufreq_gov_asswax;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_asswax)
 #endif
 
 
